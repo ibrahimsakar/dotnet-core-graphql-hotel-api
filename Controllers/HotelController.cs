ca58f5f537497818e3ee9d5838953382f1c22b7e
İbrahim Şakar <ibrahimsakar@Ibrahims-MacBook-Pro.local>
2019-11-02 23:36:33 +0300

    Project created.

---
+++ Controllers/HotelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GraphQL;
+
+namespace dotnet_core_graphql_hotel_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HotelController : ControllerBase
+    {
+        
+    }
+}
---
+++ Models/Hotel.cs
@@ -0,0 +1,9 @@
+namespace dotnet_core_graphql_hotel_api.Models
+{
+    public class Hotel {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int StarCount { get; set; }
+    }   
+}
